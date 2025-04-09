a0f41b306147f979bc527f79b508514c3a745c89
Zeteago <josetiagojtmj@gmail.com>
2025-04-09 20:23:30 -0300

    FIORI

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <div class='menuEsquerdo'></div>
+        <div class='conteudoPrincipal'></div>
+        <div class='menuDireito'></div>
+      </div>
+    </>      
+  )
+}
+
+export default App
---
+++ src/components/menuEsquerdo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function menuEsquerdo() {
+  return (
+    <div>menuEsquerdo</div>
+  )
+}
+
+export default menuEsquerdo
+
+// npm create @vite-pwa/pwa@latest
